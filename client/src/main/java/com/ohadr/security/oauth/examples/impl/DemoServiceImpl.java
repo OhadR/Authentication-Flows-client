a5ee996ba25520b4cee8229e9ffe226c849d2c4b
OhadR <ohadr.developer@gmail.com>
2013-11-28 20:41:00 +0200

    adaptations

--- client/src/main/java/com/ohadr/security/oauth/examples/impl/DemoServiceImpl.java
+++ client/src/main/java/com/ohadr/security/oauth/examples/impl/DemoServiceImpl.java
@@ -1,8 +1,5 @@
 package com.ohadr.security.oauth.examples.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestOperations;
-
 import com.ohadr.security.oauth.examples.DemoService;
 
 import java.net.URI;
@@ -11,21 +8,10 @@ import java.net.URI;
 public class DemoServiceImpl implements DemoService
 {
 
-	@Autowired
-	private RestOperations butkeDemoRestTemplate;
-
-    private String demoUrl;
-
-
-    public void setDemoUrl(String demoUrl)
-    {
-        this.demoUrl = demoUrl;
-    }
-
     @Override
     public String getTrustedMessage()
     {
-        String demo = butkeDemoRestTemplate.getForObject(URI.create(demoUrl), String.class);
+        String demo = "say hi bitch";
         return demo;
     }
 }
--- client/src/main/java/com/ohadr/security/oauth/examples/mvc/DemoController.java
+++ client/src/main/java/com/ohadr/security/oauth/examples/mvc/DemoController.java
@@ -14,7 +14,7 @@ import com.ohadr.security.oauth.examples.DemoService;
  * @author Dave Syer
  */
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("/secure/hello")
 public class DemoController
 {
 
