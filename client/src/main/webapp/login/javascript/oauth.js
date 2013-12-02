8d9d8c8642395c516a3bed19315ec35c30b0aab6
OhadR <ohadr.developer@gmail.com>
2013-12-02 10:09:27 +0200

    set new password - support userEnc in the form

--- client/src/main/webapp/login/javascript/oauth.js
+++ client/src/main/webapp/login/javascript/oauth.js
@@ -29,7 +29,7 @@ function populateResult(response)
 
 //////////////////////////////////////////////////////////////////////////////////////
 
-
+/*
 this.InitPassReq = function()
 {
    if (m_PasswordMaxLength != 100)
@@ -51,30 +51,38 @@ this.InitPassReq = function()
       m_PasswordMinNumbers=PasswordMinNumbers;
       m_PasswordMinSpecialSymbols=PasswordMinSpecialSymbols;
       m_PasswordMinUpCaseLetters=PasswordMinUpCaseLetters;
-    } catch (e) {}
+    } 
+   catch (e) {}
 }
+*/
 
-
-this.GetPassInstructions = function()
+function setEnc()
 {
-   if (m_szPassInstructions == "")
-      self.InitQuestionsAndPasswordInstructions();
-   return m_szPassInstructions;
+	// put the url parameters in an array (split by &)
+	var ar = location.search.split('&');
+
+	// remove the ? from the first option if there is an parameter
+	if(ar.length > 0)ar[0] = ar[0].substring(1, ar[0].length);
+
+	// make a reference to the form
+	var f = document.getElementById('f');
+	// loop the array we just created by splitting the location.search
+	for(var i = 0; i < ar.length; i++){
+
+		// check if the parameter has a value
+		if(ar[i].indexOf('=') != -1){
+			// get the parametername
+			var parametername = ar[i].substring(0, ar[i].indexOf('='));
+
+			// get the parametervalue
+			var parametervalue = ar[i].substring(ar[i].indexOf('=') + 1, ar[i].length);
+
+			// check if the form input exists
+			if(typeof f[parametername] != 'undefined'){
+
+				// set the form value
+				f[parametername].value = unescape(parametervalue);
+			}
+		}
+	}
 }
-
-
-
-this.InitQuestionsAndPasswordInstructions = function ()
-{
-   var szReqText = g_WDLogin.XMLHTTPRequest("/oauth-srv/createAccountPage" ,"");
-
-   if (szReqText.indexOf("OK|") == -1)
-      return;
-
-   szReqText = szReqText.substring(3);
-
-   var nPos = szReqText.indexOf("||");
-   m_szQuestions = szReqText.substring(0,nPos);
-   m_szPassInstructions = szReqText.substring(nPos+2);
-}
-
