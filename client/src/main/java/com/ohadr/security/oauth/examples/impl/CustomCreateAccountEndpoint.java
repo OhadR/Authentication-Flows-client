42a7b73d6351846bbd2384e7895abf78d4efca09
OhadR <ohadr.developer@gmail.com>
2014-06-18 21:25:05 +0300

    allow applications to overload the behavior of endpoint
    https://github.com/OhadR/oAuth2-sample/issues/32

---
+++ client/src/main/java/com/ohadr/security/oauth/examples/impl/CustomCreateAccountEndpoint.java
@@ -0,0 +1,22 @@
+package com.ohadr.security.oauth.examples.impl;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.ohadr.auth_flows.web.AuthenticationFlowsException;
+import com.ohadr.auth_flows.web.CreateAccountEndpoint;
+
+
+@Component
+public class CustomCreateAccountEndpoint extends CreateAccountEndpoint
+{
+	private static Logger log = Logger.getLogger(CustomCreateAccountEndpoint.class);
+
+	public void additionalValidations(String email, String password) throws AuthenticationFlowsException 
+	{
+		log.info("this is a custom message from CustomCreateAccountEndpoint");
+		
+		super.additionalValidations(email, password);
+	}
+
+}
