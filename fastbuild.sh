b4c7f49f98d85c1656f54c14fcf532403b948e7a
gmartins <ggmartins@gmail.com>
2015-06-25 11:29:37 -0400

    pom update

---
+++ fastbuild.sh
@@ -0,0 +1,6 @@
+echo "Running Maven Build WITHOUT tests... For development purposes only !"
+export MAVEN_OPTS=-Xmx2048m -XX:MaxPermSize=512M
+
+# mvn clean install eclipse:clean eclipse:eclipse -DskipTests -DdownloadSources=true
+
+mvn clean install -DskipTests -DdownloadSources=true
